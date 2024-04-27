e315bd766d1a042101c4c2d4f7f713c5bbfb78d2
Yermehyaw <yermehyaw@gmail.com>
2024-04-27 22:23:19 +0100

    Bubble sort algorithm

---
+++ 0-bubble_sort.c
@@ -0,0 +1,29 @@
+#include "sort.h"
+
+/**
+ * bubble_sort - sorts an array of ints with the bubble sort algo.
+ * @array: an arra5y of ints
+ * @sixe: size of the array
+ *
+ */
+
+void bubble_sort(int *array, size_t size)
+{
+	size_t i, swap;
+
+	while (proof_read_array(array, size))
+	{
+		i = 0;
+		while (i < (size - 1)) /*size - 1 to avoid passing index limit*/
+		{
+			if (array[i] >  array[i + 1])
+			{
+				swap = array[i]; /* Larger value */
+				array[i] = array[i + 1];
+				array[i + 1] = swap;
+			}
+			print_array(array, size);
+			++i;
+		}
+	}
+}
