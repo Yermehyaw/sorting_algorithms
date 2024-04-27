4b6a813f39f101922f63e77d7a04bf9adb738cba
Yermehyaw <yermehyaw@gmail.com>
2024-04-27 20:46:46 +0100

    Printing funcs

---
+++ sort.h
@@ -0,0 +1,34 @@
+#ifndef SORT_H
+#definre SORT_H
+
+/* Standard Libraries*/
+#include <stdio.h>>
+#include <stdlib.h>
+#include <unistd.h>
+
+
+/* Structure Used */
+/**
+ * struct listint_s - Doubly linked list node
+ *
+ * @n: Integer stored in the node
+ * @prev: Pointer to the previous element of the list
+ * @next: Pointer to the next element of the list
+ */
+typedef struct listint_s
+{
+    const int n;
+    struct listint_s *prev;
+    struct listint_s *next;
+} listint_t;
+
+
+
+/* Function Prototypes */
+void print_array(const int *array, size_t size);
+void print_list(const listint_t *list);
+
+
+
+
+#endif /* ifndef SORT_H */
