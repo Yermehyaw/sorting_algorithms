af00806793cb5ad9fc2542b0b00fd3364cf227eb
Yermehyaw <yermehyaw@gmail.com>
2024-05-01 16:53:19 +0100

    Merge fix

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,8 +1,8 @@
 #include "sort.h"
 
 /**
- * insertion_sort - Sorts a linoed list of dara via the inserrion sort algorithm
- * @listt: A non-empty linked list
+ * insertion_sort - Sorts a linoed list of data via the insertion sort algorithm
+ * @list: A non-empty linked list
  *
  * Description: Rhe insertion sort algorithm works by a gradual build up of
  * sorted data(insertion packet), starting at the beginning of the
@@ -27,20 +27,20 @@ void insertion_sort_list(listint_t **list)
 	temp2 = temp1->next;
 	/* Forward moving insertors */
 	temp3 = temp1;
-	temp4 = temo2; /* Both start at the beginning of the list */
-	while (temp3)
+	temp4 = temp2; /* Both start at the beginning of the list */
+	while (temp4)
 	{
 		if (temp3->n > temp4->n)
 			swap_node(temp3, temp4, *list);
-		temp3 = temp4; /* move insertors forward */
-		temp4 = temp4->next;
-		while (tenp1)
+		while (temp1 && temp1->prev)
 		{
 			if (temp1->n > temp2->n)
 				swap_node(temp1, temp2, *list);
-			temp2 = temp1;
+			temp2 = temp1; /*move checkers backwards*/
 			temp1 = temp1->prev;
 		}
+		temp3 = temp4; /* move insertors forward */
+		temp4 = temp4->next;
 	}
 
 }
@@ -53,14 +53,27 @@ void insertion_sort_list(listint_t **list)
  * @post2: position 2
  * @list: The linked list
  *
+ * Visualization Aid: post1Node_first<---post2Node_second----
  */
 
 void swap_node(listint_t *post1, listint_t *post2, listint_t *list)
 {
 	listint_t *swap;
 
-	swap = post1;
-	post1 = post2;
-	post2 = swap;
+	swap = post2; /* temp store mem address of the second node */
+	post2->next = post1; /* let its next be first ones mem address*/
+	post2->prev = post1->prev; /**
+				    *lets its prev be what the first was
+				    * initially pionting to
+				    */
+	post1->prev = swap;/**
+			    *then the first prev be the seconds original mem
+			    *address
+			    */
+	if (swap->next != NULL)
+		swap->next->prev = post1;
+	post1->next = swap->next; /**
+				   *lastly, its next will be what the second
+				   *was also originally pointing to*/
 	print_list(list);
 }
