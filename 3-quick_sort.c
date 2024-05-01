474c2dab4d9b41b99d6658b0b334bf60bf4ed916
Yermehyaw <yermehyaw@gmail.com>
2024-05-01 23:57:18 +0100

    Fix quick sort algo

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -9,7 +9,7 @@
 
 void quick_sort(int *array, size_t size)
 {
-	quick_sorter(array, 0, size);/*index to start is 0 and end is size - 1*/
+	quick_sorter(array, 0, size - 1, size);/*start is 0 & end is size - 1*/
 }
 
 
@@ -18,21 +18,20 @@ void quick_sort(int *array, size_t size)
  * @array: array of integers
  * @start: beginning of partition
  * @end: end of partition
+ * @size: size of the array
  *
  */
 
-void quick_sorter(int *array, int start, int end)
+void quick_sorter(int *array, int start, int end, int size)
 {
 	int idx;
 
 	if (start < end)
 	{
 		/* Using lomuto partioning scheme */
-		idx = lomuto_partition(array, start, end);
-		if (idx < 1) /*array is already sorted */
-			return;
-		quick_sorter(array, start, idx - 1);
-		quick_sorter(array, idx + 1, end);
+		idx = lomuto_partition(array, start, end, size);
+		quick_sorter(array, start, idx - 1, size);
+		quick_sorter(array, idx + 1, end, size);
 	}
 }
 
@@ -43,40 +42,34 @@ void quick_sorter(int *array, int start, int end)
  * @array: the array
  * @start: beginning index of the array/sub-array to partition
  * @end: ending index of the array/sub-array to partition
+ * @size: size of the array
  *
  * Return: index where the array is partitioned (int)
  */
 
-int lomuto_partition(int *array, int start, int end)
+int lomuto_partition(int *array, int start, int end, int size)
 {
 	int pivot_val, i, j, temp;
 
 	pivot_val = array[end];
 	/*j and i are the two partioning pointers*/
-	i = start;
+	i = start; /* partition index */
 	for (j = start; j <= end - 1; ++j)
 	{
-		if (array[j] >= pivot_val)/*if any valie is greater than pivot*/
+		if (array[j] <= pivot_val)/*if any value is less than pivot*/
 		{
-			i = j;/*move the pointers to he  same index*/
-			while (i <= end - 1)/**
-					*iterate till a value smaller than the
-					*value which is larger than the pivot
-					*is found
-					*/
-			{
-				if (array[i] < array[j])
-				{
-					/*swap the values*/
-					temp = array[j];
-					array[j] = array[i];
-					array[i] = temp;
-					print_array(array, end);
-					break;
-				}
-				++i;
-			}
+			/*move to ith position*/
+			temp = array[j];
+			array[j] = array[i];
+			array[i] = temp;
+/*			print_array(array, end + 1);*/
+			++i;
 		}
 	}
-	return (i); /*index where array os finally partitioned/divided */
+	/*swap value at partition idx i with val at end i.e pivot_val*/
+	temp = array[i];
+	array[i] = array[end];
+	array[end] = temp;
+	print_array(array, size);
+	return (i); /*i is at the partitioning index*/
 }
